cae9da60e9509a04fc2c65415ff427e775999ff5
chandrakant1997 <ckpatel802@gmail.com>
2020-10-14 17:03:57 +0530

    my first relise

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_scan_bluetooth/flutter_scan_bluetooth.dart';
@@ -10,12 +11,144 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String x = "0";
-
-  if ( x = "0")
-  {
-  }
   String _data = '';
+  String _a = '';
+  String _b = 'A';
+  String _c = 'B';
+  String _outtime = '';
+
+  //bool _scanning = false;
+  FlutterScanBluetooth _bluetooth = FlutterScanBluetooth();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bluetooth.devices.listen((device) {
+      setState(() {
+        _data += device.name +
+            ' (${device.address})\n' +
+            DateTime.now().toString() +
+            '\n\n';
+        if (_a == '' && _data != '') {
+          _a = _data;
+          _b = '(${device.address})';
+        }
+        _c = '(${device.address})';
+        if (_c != _b && _c != 'B') _outtime = DateTime.now().toString();
+
+        print(_data);
+        print(DateTime.now());
+      });
+    });
+
+    _bluetooth.startScan(pairedDevices: false);
+
+    /*   setState(() {
+      _scanning = true;
+    });*/
+    /* _bluetooth.scanStopped.listen((device) {
+      setState(() {
+        _scanning = false;
+        //  _data += 'scan stopped\n';
+      });
+    }*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Bluetooth detection log'),
+        ),
+        body: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(_data),
+                Text(_a + _outtime + '(Out time)'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: RaisedButton(
+                        child: Text('scan now'),
+                        onPressed: () async {
+                          try {
+                            if (true) {
+                              await _bluetooth.startScan(pairedDevices: false);
+                              debugPrint("scanning restarted");
+                              setState(() {
+                                _data = '';
+                                // _outtime = '';
+                                //_scanning = true;
+                              });
+                            }
+                            /*else {
+                            await _bluetooth.startScan(pairedDevices: false);
+                            debugPrint("scanning started");
+                          }*/
+                          } on PlatformException catch (e) {
+                            debugPrint(e.toString());
+                          }
+                        }),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    return Text(
+      ' Date ${now.day}:${now.month}:${now.year}'
+      '\t'
+      'time ${now.hour}:${now.minute}:${now.second}',
+      style: TextStyle(color: Colors.red),
+    );
+  }
+}
+
+/*class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    return Text(
+      ' Date ${now.day}:${now.month}:${now.year}'
+      '\t'
+      'time ${now.hour}:${now.minute}:${now.second}',
+      style: TextStyle(color: Colors.red),
+    );
+  }
+}*/
+
+/*
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_scan_bluetooth/flutter_scan_bluetooth.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _data = '';
+  String _a = '';
+  String _b = '';
+  String _c = '';
+  String _outTime = '';
   bool _scanning = true;
   FlutterScanBluetooth _bluetooth = FlutterScanBluetooth();
   DateTime now = DateTime.now();
@@ -26,12 +159,22 @@ class _MyAppState extends State<MyApp> {
 
     _bluetooth.devices.listen((device) {
       setState(() {
-        // _data += device.name + ' (${device.address})\n';
         _data += device.name +
             ' (${device.address})' '\n' +
             DateTime.now().toString() +
             '\n';
-        //print(_data);
+        if (_a == '' && _data != '') {
+          _a = _data;
+          _b = device.address;
+        }
+        _c = device.address;
+        if (_c != _b && _c != '') {
+          _outTime == DateTime.now();
+        }
+        // print(_outTime);
+        // _data += device.name + ' (${device.address})\n';
+
+        print(_data);
       });
     });
 
@@ -39,16 +182,20 @@ class _MyAppState extends State<MyApp> {
       pairedDevices: true,
     );
 
+*/
 /*    setState(() {
       _scanning = true;
-    });*/
+    });*/ /*
+
+*/
 /*    _bluetooth.scanStopped.listen((device) {
       setState(() {
         _scanning = true;
         _data += 'scan stopped\n';
         print(DateTime.now().toString());
       });
-    }*/
+    }*/ /*
+
   }
 
   @override // no need
@@ -72,7 +219,11 @@ class _MyAppState extends State<MyApp> {
                 Row(
                   children: [
                     Text(
-                      _data,
+                      _a,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      _outTime,
                       style: TextStyle(fontSize: 18),
                     ),
                     SizedBox(
@@ -99,16 +250,18 @@ class _MyAppState extends State<MyApp> {
                               });
                             }
 
-                            /*else {
+                            */
+/*else {
                             await _bluetooth.startScan(pairedDevices: false);
                             debugPrint("scanning started");
-                          }*/
+                          }*/ /*
+
                           } on PlatformException catch (e) {
                             debugPrint(e.toString());
                           }
                         }),
                   ),
-                )
+                ),
               ],
             ),
           ],
@@ -117,3 +270,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+*/
